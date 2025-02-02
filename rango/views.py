cd7bde766faa1be34f65453a8cb27c0ce3bb47a8
Uyen Tran <tranphuonguyen1011@gmail.com>
2025-02-02 15:56:31 +0000

    Add static and media directories

--- rango/views.py
+++ rango/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!<br><a href='/rango/about/'>About</a>")
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page.<br><a href='/rango/'>Index</a>")
+    context_dict = {'name': 'Uyen'}
+    return render(request, 'rango/about.html',context=context_dict)
+
+def secret(request):
+    context_dict = {
+        'secret1': 'Hello this is secret number 1.', 
+        'secret2': 'Hello this is secret number 2.'
+        }
+    return render(request, 'rango/secret.html', context=context_dict)
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
-from django.urls import include
+from django.urls import path, include
 from rango import views
 
 urlpatterns = [
@@ -25,3 +26,7 @@ urlpatterns = [
     # The above maps any URLs starting with rango/ to be handled by rango.
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
